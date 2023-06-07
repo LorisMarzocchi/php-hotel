691f07c277ea937aa849746a567e14f6b2e09d7d
LorisMarzocchi <ml77182a@gmail.com>
2023-06-07 18:54:23 +0200

    addstyle

--- index.php
+++ index.php
@@ -94,29 +94,40 @@ if (empty($parkingFilter) && empty($voteFilter)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP-HOTEL</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
 </head>
+
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-size: 1.2rem;
+    }
+
+</style>
 
 <body>
     <div class="container mt-5">
     <form action="" method="GET">
         <div>
-            <label for="parking">Parking</label>
+            <label class="me-2" for="parking">Parking</label>
             <select id="parking" name="parking">
                 <option name="" selected>Seleziona</option>
                 <option value="Con parcheggio">Con parcheggio</option>
                 <option value="Senza parcheggio">Senza parcheggio</option>
             </select>
-            <label for="vote">Voto</label>
+            <label class="ms-5 me-2" for="vote">Voto</label>
             <input type="number" name="vote" id="vote">
 
-            <button class="btn btn-primary" type="submit">FILTRA</button>
+            <button class="btn btn-primary ms-5" type="submit">FILTRA</button>
 		</div>
     </form>
-        <table class="table">
+        <table class="table mt-5">
             <thead>
-                <tr>
+                <tr class="table-dark">
                     <th scope="col">Nome</th>
                     <th scope="col">Descrizione</th>
                     <th scope="col">Parcheggio</th>
